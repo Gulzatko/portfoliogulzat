e6a98e02f39f0dbb5000804502f45cdb4713e5eb
Gulzatko <Mamajanovna86@gmail.com>
2024-10-08 23:02:14 +0400

    completed navbar section, by doing responsive, interacting icons

---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { useState } from "react";
+import { FaBars, FaReact } from "react-icons/fa";
+import { HiX } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import "./styles.scss";
+
+// const data = [
+//   {
+//     label: "HOME",
+//     to: "/",
+//   },
+//   {
+//     label: "ABOUT ME",
+//     to: "/about",
+//   },
+//   {
+//     label: "SKILLS",
+//     to: "/skills",
+//   },
+//   {
+//     label: "RESUME",
+//     to: "/resume",
+//   },
+//   {
+//     label: "PORTFOLIO",
+//     to: "/portfolio",
+//   },
+//   {
+//     label: "CONTACT",
+//     to: "/contact",
+//   },
+// ];
+
+const Navbar = () => {
+  const [toggleIcon, setToggleIcon] = useState(false);
+
+  const handleToggleIcon = () => {
+    setToggleIcon(!toggleIcon);
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar__container">
+          <Link to={"/"} className="navbar__container__logo">
+            <FaReact size={30} />
+          </Link>
+        </div>
+        <ul className={`navbar__container__menu ${toggleIcon ? 'active' : ''}`}>
+          <li className="navbar__container__menu__item">
+            <Link
+              className="navbar__container__menu__item__links"
+              to={"/home"}
+            ></Link>
+          </li>
+          <li className="navbar__container__menu__item">
+            <Link
+              className="navbar__container__menu__item__links"
+              to={"/about"}
+            >
+              About
+            </Link>
+          </li>
+          <li className="navbar__container__menu__item">
+            <Link
+              className="navbar__container__menu__item__links"
+              to={"/contact"}
+            >
+              Contact
+            </Link>
+          </li>
+          <li className="navbar__container__menu__item">
+            <Link
+              className="navbar__container__menu__item__links"
+              to={"/portfolio"}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li className="navbar__container__menu__item">
+            <Link
+              className="navbar__container__menu__item__links"
+              to={"/resume"}
+            >
+              Resume
+            </Link>
+          </li>
+          <li className="navbar__container__menu__item">
+            <Link
+              className="navbar__container__menu__item__links"
+              to={"/skills"}
+            >
+              Skills
+            </Link>
+          </li>
+        </ul>
+        <div className="nav-icon" onClick={handleToggleIcon}>
+          {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
