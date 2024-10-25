051a58c6a5293a51c9c83eff5f03579ddbf0a667
Gulzatko <Mamajanovna86@gmail.com>
2024-10-25 13:49:05 +0400

    added VerticalTimeline component by importing react-vertical-timeline-component and react-vertical-timeline-component/style.min.css

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,45 @@
+export const data = {
+    experience:[
+        {
+            title: "Frontend Developer-internship",
+            subtitle:"Awwaltech Pvt.Ltd",
+            description:
+            "IT solutions company"
+        },
+        {
+            title: "Front-end Developer intern on remote",
+            subtitle:"Neobis in Bishkek, Kyrgyzstan",
+            description:
+            "Constalting for IT projects"
+        },
+        {
+            title: "Front-end Developer intern on remote",
+            subtitle:"Emlosoft in Chicago, USA",
+            description:
+            "Application for user of truck driver"
+        },
+       
+    ],
+    educaton:[
+        {
+            title: "Frontend Developer",
+            subtitle:"Seytech",
+            description:
+            "Software engeneering Bootcamp"
+        },
+        {
+            title: "Faculty of World Languages",
+            subtitle:"Osh State University",
+            description:
+            "Univercity degree"
+        },
+        {
+            title: "Higher secondary",
+            subtitle:"Razzakova school in Osh, Kyrgyzstan",
+            description:
+            "11th Grade"
+        },
+       
+    ],
+
+}
